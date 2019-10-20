3373f74247ad218fd074a206d7f16a21f661684f
Mustafa Zuvin <zuvin97@gmail.com>
2019-10-20 10:24:31 +0300

    Haber Engelle
    gizli modda kullanılması tavsiye edilir.

--- haberengelle.js
+++ haberengelle.js
@@ -1,37 +1,25 @@
-javascript:
+javascript: habersiteleri = ["trthaber.com", "mynet.com", "haberler.com", "ensonhaber.com", "ahaber.com.tr", "ntv.com.tr", "milliyet.com.tr", "sondakika.com", "internethaber.com", "t24.com.tr", "memurlar.net", "yenisafak.com", "star.com.tr", "fotomac.com.tr", "sabah.com.tr", "hurriyet.com.tr", "sozcu.com.tr", "haber7.com", "haberturk.com", "cnnturk.com", "aksam.com.tr", "takvim.com.tr"]
 
-var habersiteleri=[
-"trthaber.com",
-"mynet.com",
-"haberler.com",
-"ensonhaber.com",
-"ahaber.com.tr",
-"ntv.com.tr",
-"milliyet.com.tr",
-"sondakika.com",
-"internethaber.com",
-"t24.com.tr",
-"memurlar.net",
-"yenisafak.com",
-"star.com.tr",
-"fotomac.com.tr",
-"sabah.com.tr",
-"hurriyet.com.tr",
-"sozcu.com.tr",
-"haber7.com",
-"haberturk.com",
-"cnnturk.com",
-"aksam.com.tr",
-"takvim.com.tr"]
-var engelle="";
-for (var key in habersiteleri){
-    engelle+=" -site:"+habersiteleri[key];
+var engelle = "";
+
+for (var key in habersiteleri) {
+    engelle += " -site:" + habersiteleri[key];
 }
 
-javascript: if (document.URL.search("google.com") < 0) {
-    alert("Haber Engelleyici BookMarklet");
-    location.href = "https://google.com";
-} else {
+if (document.URL.search("google.com") < 0) {
     location.href = "https://google.com/search?q=" + escape(prompt('Googleda arama yapin:').replace(" ", "+")) + engelle;
+    
+} else {
+    /*https://stackoverflow.com/a/8486146*/
+    var regex = /[?&]([^=#]+)=([^&#]*)/g, url = window.location.href, params = {}, match;
+    while (match = regex.exec(url)) {
+        params[match[1]] = match[2];
+    }
+    if(params['q']===undefined){
+        location.href = "https://google.com/search?q=" + escape(prompt('Googleda arama yapin:').replace(" ", "+")) + engelle;
+        
+    }
+    else{
+        location.href = "https://google.com/search?q=" + params['q'] + engelle;  
+    }   
 }
-save();
